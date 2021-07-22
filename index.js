1ec56f8eb82296b72e665df4d716592bdf951024
valabji <valabji@gmail.com>
2021-07-22 18:45:42 +0200

    new Method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const PORT = process.env.PORT || 880;
+const PORT = process.env.PORT || 80;
 console.log("Valabji Port is " + PORT)
-
+/* 
 const NetcatServer = require('netcat/server')
 const NetcatClient = require('netcat/client')
 const nc = new NetcatServer()
@@ -10,4 +10,58 @@ nc.port(PORT).k().listen().on('data', function (rinfo, data) {
     console.log('Got', data.toString(), 'from', rinfo)
     nc2.addr('google.com').port(80).connect()
     nc.proxy(nc2.stream())
-})
+}) */
+
+var http = require('http');
+var sys = require('sys');
+
+server = http.createServer(function(request, response) {
+     sys.log(request.connection.remoteAddress + ": " + 
+        request.method + " " + 
+        request.url);
+
+      var proxy = http.createClient(80, request.headers['host']);
+
+      var proxy_request = proxy.request(request.method, request.url, 
+         request.headers);
+
+      proxy_request.addListener('response', function (proxy_response) {
+         proxy_response.addListener('data', function(chunk) {
+            // sys.log("response: " + chunk);
+            response.write(chunk, 'binary');
+         });
+
+         proxy_response.addListener('end', function() {
+            response.end();
+         });
+
+         response.writeHead(proxy_response.statusCode, 
+            proxy_response.headers);
+      });
+
+      // prevent connection reset from killing the process
+      request.socket.removeAllListeners('error'); 
+
+      request.addListener('data', function(chunk) {
+            sys.log("request: " + chunk);
+            proxy_request.write(chunk, 'binary');
+         });
+      request.addListener('end', function() {
+            proxy_request.end();
+         });
+      request.socket.addListener('error', function(err) {
+            sys.error("error occured in request socket\n");
+            sys.inspect(err);
+         });
+
+
+   });
+
+server.listen(PORT);
+
+process.addListener("unhandledException", function (err) { 
+      sys.error("something bad happened!\n");
+      sys.inspect(err);
+      sys.error("restarting server");
+      server.listen(PORT);
+});
