4ef97ff9d7976ecbece58e17f096f5a1ca6b3f01
valabji <valabji@gmail.com>
2021-07-22 15:04:22 +0200

    Heroku port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-
+const PORT = process.env.PORT || 3000;
+console.log("Valabji Port is "+PORT)
 app.get('/',(req,res)=>{
     res.status(200).send("<h1>Valabji First Heroku App</h1>")
 })
 
-app.listen(80)
+app.listen(PORT)
